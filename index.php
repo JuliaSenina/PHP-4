fc7edcf45c4e4498b8c9dcd6d628dfe5ca025398
Сенина Юлия Александровна <y.senina@ens.ru>
2023-03-22 08:23:17 +0300

    commit

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 
 $name = 'иван';
-$surname = 'иванов';
-$patronymic = 'иванович';
+$surname = 'сидоров';
+$patronymic = 'петрович';
 
 $fullName = $surname . ' ' . $name . ' ' . $patronymic;
 $fullName = mb_convert_case($fullName, MB_CASE_TITLE, "UTF-8");
 
-$patronymic = mb_strtoupper(substr($name,0,2), "utf-8");
-$name = mb_strtoupper(substr($name,0,2), "utf-8");
-$surname = mb_convert_case($surname, MB_CASE_TITLE, "UTF-8");
-$surnameAndInitials = $surname . ' ' . $name .'.'.$patronymic.'.';
+$strSurnameAndInitials = $surname.' '.mb_substr($name, 0, 1).'.'.mb_substr($patronymic, 0, 1).'.';
+$surnameAndInitials = preg_replace_callback('/(^|\.|\s){1}\w/u',
+  static function($m) {
+    return mb_convert_case($m[0], MB_CASE_UPPER, "UTF-8");
+  },
+  $strSurnameAndInitials);
 
-$surname = mb_substr($surname, 0, 1, "UTF-8");
-$fio = $surname.$name.$patronymic;
+$strFIO = mb_substr($surname, 0, 1).mb_substr($name, 0, 1).mb_substr($patronymic, 0, 1);
+$fio = mb_convert_case($strFIO, MB_CASE_UPPER, "UTF-8");
 
 echo "Полное имя: '$fullName' ".PHP_EOL;
 echo "Фамилия и инициалы: '$surnameAndInitials'".PHP_EOL;
